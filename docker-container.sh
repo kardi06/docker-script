10f8ee442ec4500da31f9d9070735f4aaa2bfa07
Kardi <kgland06@gmail.com>
2024-10-05 00:02:12 +0700

    envionment variable

--- docker-container.sh
+++ docker-container.sh
@@ -27,3 +27,7 @@ docker container exec -i -t redis bin/bash
 
 #! port Forwarding
 docker container create --name contohnginx --publish 8080:80 nginx:latest
+
+#! Environment Variable
+docker container create --name contohmongo --publish 27017:27017 --env MONGO_INITDB_ROOT_USERNAME=admin 
+--env MONGO_INITDB_ROOT_PASSWORD=password mongo:latest
