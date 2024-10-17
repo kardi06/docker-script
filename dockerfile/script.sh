c0ce2f089e4bf70077c21f3ac53910afd118f4b3
Kardi <kgland06@gmail.com>
2024-10-18 03:07:10 +0700

    add instruction

--- dockerfile/script.sh
+++ dockerfile/script.sh
@@ -1,10 +1,29 @@
-#FROM instruction
+# FROM instruction
 docker built -t kardi/from from
 #from yang terakhir itu nama folder tempat dockerfilenya berada
 
 docker image ls
 
-#RUN instruction
+# RUN instruction
 docker build -t kardi/run:latest run
 
 docker build -t kardi/run:1.0 run --progress=plain --no-cache
+
+# COMMAND instruction
+docker build -t kardi/command command
+
+docker image inspect kardi/command
+
+# LABEL Instruction
+docker build -t khannedy/label label
+
+docker image inspect khannedy/label
+
+# ADD Instruction
+docker build -t khannedy/add add
+
+docker container create --name add khannedy/add
+
+docker container start add
+
+docker container logs add
